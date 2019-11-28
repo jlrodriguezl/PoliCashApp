440a662f6915f45fccededbf9d882522665a9709
Jose Rodriguez <jrodriguez.51c@gmail.com>
2019-11-27 20:02:23 -0500

    Rutas login y ahorrar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,8 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  { path: 'login', loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)},
+  { path: 'ahorrar', loadChildren: () => import('./pages/ahorrar/ahorrar.module').then( m => m.AhorrarPageModule)}
 ];
 
 @NgModule({
