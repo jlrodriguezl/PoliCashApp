32edcb4211c8339e444563832d510799372ad311
Rigoberto Ayala <rigoberto318722@gmail.com>
2019-12-02 18:41:44 -0500

    entrego vista principal

---
+++ src/app/pages/pricipal/pricipal.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pricipal',
+  templateUrl: './pricipal.page.html',
+  styleUrls: ['./pricipal.page.scss'],
+})
+export class PricipalPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
