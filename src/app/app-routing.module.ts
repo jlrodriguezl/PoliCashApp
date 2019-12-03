c33dafd8e25302f21b709f8bf593aa856e78f40f
Jose Rodriguez <jrodriguez.51c@gmail.com>
2019-12-02 19:28:49 -0500

    Modificación Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,10 @@ const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
   { path: 'login', loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)},
-  { path: 'ahorrar', loadChildren: () => import('./pages/ahorrar/ahorrar.module').then( m => m.AhorrarPageModule)}
+  { path: 'ahorrar', loadChildren: () => import('./pages/ahorrar/ahorrar.module').then( m => m.AhorrarPageModule)},
+  { path: 'pricipal', loadChildren: () => import('./pages/pricipal/pricipal.module').then( m => m.PricipalPageModule)},
+  { path: 'retirar', loadChildren: () => import('./pages/retirar/retirar.module').then( m => m.RetirarPageModule)},
+  { path: 'transferir', loadChildren: () => import('./pages/transferir/transferir.module').then( m => m.TransferirPageModule)}
 ];
 
 @NgModule({
