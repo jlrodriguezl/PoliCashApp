89d9fc2e76e8eb9013b3fe5d02a40de9527a9ff7
oscargiovanny <oscargiovannyrodriguez@gmail.com>
2019-12-05 08:39:06 -0500

    VIsta QR Pagos (alert, boton y fab)

--- src/app/pages/agregar/agregar.page.ts
+++ src/app/pages/agregar/agregar.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-agregar',
@@ -6,9 +7,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./agregar.page.scss'],
 })
 export class AgregarPage implements OnInit {
+  constructor(public alertController: AlertController) { }
+  async presentAlertPrompt() {
+    const alert = await this.alertController.create({
+      header: 'Bienvenido! Para continuar LLena la siguente informacion :)',
+      inputs: [
+        {
+          name: 'Codigo de Retiro',
+          type: 'text',
+          placeholder: 'Ingrese el codigo'
+        },
+        {
+          name: 'Valor  Retiro',
+          type: 'text',
+          id: 'name2-id',
+          placeholder: 'Valor Retiro'
+        },
+        {
+          name: 'Condiciones: La accion de utilizar este medio se debe a su autprizacion para el manejo de la informacion si tiene dudas comuniquece con nuestro asesores '
+        },
+       
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: () => {
+            console.log('Confirm Cancel');
+          }
+        }, {
+          text: 'Ok',
+          handler: () => {
+            console.log('Confirm Ok');
+          }
+        }
+      ]
+    });
 
-  constructor() { }
-
+    await alert.present();
+  }
   ngOnInit() {
   }
 
