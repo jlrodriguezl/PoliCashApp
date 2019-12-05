3c08fb467970fa29286cc94344ebd1864ad1e1f2
Jose Rodriguez <jrodriguez.51c@gmail.com>
2019-12-04 19:18:37 -0500

    Servicio General

---
+++ src/app/services/general.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GeneralService {
+
+  constructor() { }
+
+  guardarStorage(){
+    //localStorage.setItem('data', JSON.stringify())
+  }
+}
